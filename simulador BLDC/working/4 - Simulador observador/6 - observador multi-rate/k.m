dc3ef15f11762dbf82bd326a9c00a8e427863dba
Juliano <julianotusi@gmail.com>
2014-09-30 10:23:17 -0300

    No meio do debug do observador multirate
    Nesse ponto eu desenvolvi:
    - observador multirate Ax+Bu+Ly
    - observador multirate que as vezes atualiza com Ax+Bu e quando
    tem medida de sensor atualiza com Ax+Bu+Ly. Ele usa o deltaT
    entre as medidas

--- simulador BLDC/working/4 - Simulador observador/6 - observador multi-rate/k.m
+++ simulador BLDC/working/4 - Simulador observador/6 - observador multi-rate/k.m
@@ -8,10 +8,25 @@
 % subplot(3,1,3); 
 % plot(1./deltaT(3:100707));
 
+% figure;
+% plot(tout,simx_hat(:,1));
+% hold on; 
+% plot(tout,simx,'r'); 
+% axis([0 10 -450 700]);
+% plot(tout,trigger_sens*100,'r')
+% plot(tout,trigger*200,'k');
+
+
+obsv = zeros(size(simL));
+obsv(find(trigger==1),:) = simL(find(trigger==1),:);
+obsv(find(trigger_sens==1),:) = simL(find(trigger_sens==1),:);
+
+
 figure;
 plot(tout,simx_hat(:,1));
 hold on; 
 plot(tout,simx,'r'); 
 axis([0 10 -450 700]);
-plot(tout,trigger_sens*100,'r')
+plot(tout,trigger_sens*100,'r');
+%plot(tout,obsv(:,1)*10,'g');
 plot(tout,trigger*200,'k');
